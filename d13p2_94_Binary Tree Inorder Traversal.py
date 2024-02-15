db11699afd385e1baf96b2b9134285053ff8f121
Yi Chien Lee <yician.lee@gmail.com>
2024-02-15 16:18:42 -0700

    practice day 14

---
+++ d13p2_94_Binary Tree Inorder Traversal.py
@@ -0,0 +1,23 @@
+from typing import List, Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        
+        if root:
+            self.dfs(root, res)
+        return res
+    
+    def dfs(self, node, res):
+        if node.left:
+            self.dfs(node.left, res)
+        if node.right:
+            self.dfs(node.right, res)
+        res.append(node.val)
