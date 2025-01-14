11156ec1d43d25587b66222c9c45930ec06d1419
amandayclee <yician.lee@gmail.com>
2025-01-13 21:40:09 -0700

    practice leetcode

--- blind75/array_128. Longest Consecutive Sequence.py
+++ blind75/array_128. Longest Consecutive Sequence.py
@@ -1,6 +1,5 @@
 from typing import List
 
-
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         nums_set = set(nums)
@@ -16,3 +15,14 @@ class Solution:
                 max_length = max(length, max_length)
                 
         return max_length
+    
+
+if __name__ == "__main__":
+    solution = Solution()
+    
+    test_cases = [
+        {"input": ([100,4,200,1,3,2]), "expected_output": (4)},
+        {"input": ([0,3,7,2,5,8,4,6,0,1]), "expected_output": (9)},
+    ]
+    for test_case in test_cases:
+        assert solution.longestConsecutive(test_case["input"]) == test_case["expected_output"]
--- blind75/array_238. Product of Array Except Self.py
+++ blind75/array_238. Product of Array Except Self.py
@@ -1,7 +1,31 @@
 from typing import List
 
+class Solution1:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        total_product = 1
+        zero_count = 0
+        
+        for number in nums:
+            if number != 0:
+                total_product *= number
+            else:
+                zero_count += 1
+        
+        if zero_count > 1:
+            return [0] * len(nums)
+        
+        result = [0] * len(nums)
+        
+        for index, current_number in enumerate(nums):
+            if zero_count:
+                result[index] = 0 if current_number != 0 else total_product
+            else:
+                result[index] = total_product // current_number
+                
+        return result
+    
 
-class Solution:
+class Solution2:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         result = []
 
@@ -19,22 +43,30 @@ class Solution:
             
         return result
     
-Solution().productExceptSelf([1, 2, 3, 4])
-
-
-class Solution:
+class Solution3:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # take result as prefix output
-
-        result = [1] * len(nums)
-
-        for i in range(1, len(nums)):
-            result[i] = result[i - 1] * nums[i - 1]
-
-        suffix = 1
-
+        res = [1] * (len(nums))
+        
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+            
+        postfix = 1
         for i in range(len(nums) - 1, -1, -1):
-            result[i] = suffix * result[i]
-            suffix *= nums[i]
+            res[i] *= postfix
+            postfix *= nums[i]
+            
+        return res
 
-        return result
+if __name__ == "__main__":
+    solution = Solution2()
+    
+    test_cases = [
+        {"input": ([1,2,3,4]), "expected_output": [24,12,8,6]},
+        {"input": ([-1,1,0,-3,3]), "expected_output": [0,0,9,0,0]},
+    ]
+    for test_case in test_cases:
+        assert solution.productExceptSelf(test_case["input"]) == test_case["expected_output"]
+        
+
